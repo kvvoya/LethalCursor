37ba7fcb1f7ac3c65945fe355771d0cf11cf4d4e
kvvoya <suruhakken@gmail.com>
2024-02-18 20:54:48 +0300

    initial commit

---
+++ LethalCursor/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+
+
+namespace LethalCursor
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private ConfigEntry<string> cursorPath;
+        private ConfigEntry<int> hotspotX;
+        private ConfigEntry<int> hotspotY;
+        private ConfigEntry<bool> disableModEntirely;
+
+        private void Awake()
+        {
+            cursorPath = Config.Bind("General", "CursorPath", "BepInEx/plugins/LethalCompanyTemplate/Resources/cursor.png", "The path to an image to use as a cursor.");
+            hotspotX = Config.Bind("General", "HotSpotX", 0, "The X value of the cursor hotspot");
+            hotspotY = Config.Bind("General", "HotSpotY", 0, "The Y value of the cursor hotspot");
+            disableModEntirely = Config.Bind("General.Toggles", "DisableMod", false, "Just use the default cursor.");
+
+            if (disableModEntirely.Value)
+            {
+                Logger.LogWarning("You have disabled the mod in the configuration file. The mod will use the default cursor");
+                return;
+            }
+
+            Logger.LogInfo("LethalCompanyTemplate has been loaded! The default image is the white pixel, refer to the instructions or to the config for more info!");
+            Logger.LogInfo($"Loading the image from {cursorPath.Value}");
+
+            Texture2D cursorTexture = new Texture2D(32, 32);
+            byte[] fileData = File.ReadAllBytes(cursorPath.Value);
+            cursorTexture.LoadImage(fileData);
+
+            if (cursorTexture != null)
+            {
+                Logger.LogInfo("Cursor is loaded succesfully!");
+                Cursor.SetCursor(cursorTexture, new Vector2(hotspotX.Value, hotspotY.Value), CursorMode.Auto);
+            }
+            else
+            {
+                Logger.LogError("Something went wrong while loading the cursor! Using the default cursor instead!");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Lethal Cursor
+
+This is a mod that allows you to change your cursor in the UI elements to whatever image you want! 🌟
+
+I made this mod for my bestie Yzis, because she had a weird bug that made it so the cursor is always visible.
+
+[Download!]()
+
+## How to use
+
+
+1. The default cursor this mod will use as a replacement is just one white pixel. There are two ways to change it. Go to the configuration of the mod and set the path of your image there. Alternatively, go to the mod's folder and replace the file called cursor.png but keep the default path in the config file.
+2. By default, the hotspot is set on the top left corner. You can change the hotspot coordinates in the configuration of a mod. I advise you to have your cursor of 32x32 size.
+3. If you wish to disable the mod and see the default picture again, you can disable the mod in the configuration as well
+
+## Credits
+
+- thank you, Yzis, for the icon of the mod <3
+
+###### v1.0
